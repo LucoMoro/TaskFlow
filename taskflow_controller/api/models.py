b77f8e269c9615cce6e5b21d8c0ca8baa50e2dd7
LucoMoro <morelli.2000luca@gmail.com>
2023-12-07 19:22:03 +0100

    Test back-end set-up

--- taskflow_controller/api/models.py
+++ taskflow_controller/api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+import string
+import random
+
+#randomly generates a name for the Team
+def generate_unique_name():
+    length = 6
+
+    while True:
+        name = ''.join(random.choices(string.ascii_uppercase, k=length))
+        if Team.objects.filter(name=name).count() == 0:
+            break
+
+    return name
 
 # Create your models here.
+class Team(models.Model):
+    name = models.CharField(max_length=25, default="", unique=True)
+    project_manager = models.CharField(max_length=50, unique=True)
+    team_member = models.CharField(max_length=50, unique=False)
+    num_of_members = models.IntegerField(null=False, default=5)
+    created_at = models.DateTimeField(auto_now_add=True)
--- taskflow_controller/api/urls.py
+++ taskflow_controller/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import main #made up function
+from .views import TeamView #made up function
 
 urlpatterns = [
-    path('', main)
+    path('', TeamView.as_view())
 ]
